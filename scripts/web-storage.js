3e520c39cc25698c394952ddd3c9fb31c06a4d11
rrtcastro <rrtcastro@gmail.com>
2016-05-09 23:32:11 +0800

    Javascript inserting sticky notes at top, middle, and bottom of col

--- scripts/web-storage.js
+++ scripts/web-storage.js
@@ -1,8 +1,39 @@
+var stickyNoteFrontTemplate = '<article class="sticky-note"> \
+  <div class="add-new"><p>+</p></div> \
+  <div class="sticky-note-content">'
+var stickyNoteBackTemplate = '</div></article>'
+var stickyNotes = [[], [], [], []]
+
 $(document).ready(function(){
   $(".col-content").each(function(index){
-    if($(this).children().length == 0){
-      $(this).html("<div class='add-col'><p>+</p></div>")
+    if($(this).children().length == 1){
+      $(this).append("<div class='add-col'><p>+</p></div>")
     }
   })
-  
+
+  $(".add-col").click(function(){
+    var columnIndex = $(this).siblings("input").val() - 1
+    stickyNotes[columnIndex].push("Hello World")
+
+    $(this).parent().append(stickyNoteFrontTemplate + "Hello World" + stickyNoteBackTemplate)
+    $(this).parent().append("<div class='add-end-col'><p>+</p></div>")
+    $(this).remove()
+  })
+
+  $(".add-end-col").click(function(){
+    var columnIndex = $(this).siblings("input").val() - 1
+    stickyNotes[columnIndex].push("Hello World")
+
+    $(this).before(stickyNoteFrontTemplate + "Hello World" + stickyNoteBackTemplate)
+  })
+
+  $(".add-new").click(function(event){
+    columnIndex = $(this).parent().siblings("input").val() - 1
+    stickyNotes[columnIndex].push("Hello World")
+
+    element = $(this).parent().clone(true)
+    element.children(".sticky-note-content").html("Hello World")
+    $(this).parent().before(element)
+  })
+
 })
