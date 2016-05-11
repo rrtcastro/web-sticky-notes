31e3f959aeee10a914b9687545d067235465c8eb
rrtcastro <rrtcastro@gmail.com>
2016-05-11 22:45:13 +0800

    Added add, edit, delete to localStorage functionality. Data now persist through localStorage

--- scripts/web-storage.js
+++ scripts/web-storage.js
@@ -1,18 +1,24 @@
-myStorage = localStorage;
+if(!localStorage.getItem('stickyNotes')){
+  localStorage.setItem('stickyNotes', JSON.stringify([[], [], [], []]))
+  var stickyNotes = [[], [], [], []]
+}
+else{
+  stickyNotes = JSON.parse(localStorage.getItem('stickyNotes'))
+}
 
 var stickyNoteFrontTemplate = '<article class="sticky-note"> \
   <input type="hidden" value="0"> \
   <div class="sticky-note-content">'
 var stickyNoteBackTemplate = '</div> \
   <div class="delete-triangle"></div></article>'
-var stickyNotes = [[], [], [], []]
 
 function addDeleteHandler(e){
-  var columnIndex = e.parent().siblings("input").val() - 1
+  var columnIndex = e.parent().siblings("input").val()
   var noteIndex = e.siblings("input").val()
   var col = e.parent().parent()
 
   stickyNotes[columnIndex].splice(noteIndex, 1)
+  localStorage.setItem('stickyNotes', JSON.stringify(stickyNotes))
   e.parent().remove()
 
   if(col.children().length > 3){
@@ -32,6 +38,11 @@ function addEditHandler(e){
     var code = (event.keyCode ? event.keyCode : event.which)
     if (!event.shiftKey && code == 13) {
       var newValue = e.prev().val()
+      var colIndex = e.parent().siblings("input").val()
+      var noteIndex = e.siblings("input").val()
+      stickyNotes[colIndex][noteIndex] = [newValue, 0]
+      localStorage.setItem('stickyNotes', JSON.stringify(stickyNotes))
+
       e.html(newValue).show()
       e.prev().remove()
     }
@@ -42,6 +53,10 @@ function addEditHandler(e){
 }
 function addStrikeThrough(event, element){
   if(event.which == 2){
+    var colIndex = element.parent().siblings("input").val()
+    var noteIndex = element.siblings("input").val()
+    stickyNotes[colIndex][noteIndex][1] = !stickyNotes[colIndex][noteIndex][1]
+    localStorage.setItem('stickyNotes', JSON.stringify(stickyNotes))
     element.toggleClass("text-linethrough")
   }
 }
@@ -52,19 +67,31 @@ function addFunction(e){
 }
 
 $(document).ready(function(){
-  $(".col-content").each(function(index){
-    if($(this).children().length == 3){
-      $(this).children(".add-end-col").hide()
+
+  stickyNotes.forEach(function(col, colIndex){
+    if(col.length == 0){
+      $(".col-content").eq(colIndex).children(".add-end-col").hide()
     }
     else{
-      $(this).children(".add-col").hide()
+      var bottom = $(".col-content").eq(colIndex).children(".add-end-col")
+      $(".col-content").eq(colIndex).children(".add-col").hide()
+      stickyNotes[colIndex].forEach(function(note, noteIndex){
+        bottom.before(stickyNoteFrontTemplate + note[0] + stickyNoteBackTemplate)
+        bottom.prev().children("input").val(noteIndex)
+        addFunction(bottom.prev())
+
+        if(note[1])
+          bottom.prev().children(".sticky-note-content").addClass("text-linethrough")
+
+      }, bottom)
     }
   })
 
   $(".add-col").click(function(){
-    var columnIndex = $(this).siblings("input").val() - 1
+    var columnIndex = $(this).siblings("input").val()
     var note = prompt("What's on your mind")
-    stickyNotes[columnIndex].push(note)
+    stickyNotes[columnIndex].push([note, false])
+    localStorage.setItem('stickyNotes', JSON.stringify(stickyNotes))
 
     $(this).after(stickyNoteFrontTemplate + note + stickyNoteBackTemplate)
     addFunction($(this).next())
@@ -74,9 +101,11 @@ $(document).ready(function(){
   })
 
   $(".add-end-col").click(function(){
-    var columnIndex = $(this).siblings("input").val() - 1
+    var columnIndex = $(this).siblings("input").val()
     var note = prompt("What's on your mind")
-    stickyNotes[columnIndex].push(note)
+    stickyNotes[columnIndex].push([note, false])
+    localStorage.setItem('stickyNotes', JSON.stringify(stickyNotes))
+
 
     $(this).before(stickyNoteFrontTemplate + note + stickyNoteBackTemplate)
     $(this).prev().children("input").val(stickyNotes[columnIndex].length - 1)
